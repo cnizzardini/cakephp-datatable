93cecd7da88dc27f0caf6dbd776d81c353c1c297
chris <chris@cnizz>
2013-05-25 10:59:06 -0600

    examples

--- test/app/Controller/CitiesController.php
+++ test/app/Controller/CitiesController.php
@@ -5,7 +5,18 @@ class CitiesController extends AppController {
     
     public $components = array('DataTable');
     
+    
     public function index(){
+        if($this->RequestHandler->responseType() == 'json') {
+            $this->paginate = array(
+                'fields' => array('City.id', 'City.name', 'City.state_id','City.population'),
+            );
+            $this->set('cities', $this->DataTable->getResponse());
+            $this->set('_serialize','cities');
+        }
+    }
+    
+    public function linkable(){
         if($this->RequestHandler->responseType() == 'json') {
             $this->paginate = array(
                 'fields' => array('City.id', 'City.name', 'City.population'),
@@ -20,9 +31,83 @@ class CitiesController extends AppController {
             $this->DataTable->emptyElements = 1;
             $this->set('cities', $this->DataTable->getResponse());
             $this->set('_serialize','cities');
-        } 
-        else{
+        }
+    }
+    
+    public function containable(){
+        if($this->RequestHandler->responseType() == 'json') {
+            $this->paginate = array(
+                'fields' => array('City.id', 'City.name', 'City.population'),
+                'contain' => array(
+                    'State' => array(
+                        'fields' => array('name')
+                    )
+                )
+            );
+            
+            $this->DataTable->fields = array('City.id','City.name','State.name','City.population');
+            $this->DataTable->emptyElements = 1;
+            $this->set('cities', $this->DataTable->getResponse());
+            $this->set('_serialize','cities');
+        }
+    }
+    
+    public function concat(){
+        if($this->RequestHandler->responseType() == 'json') {
+            $this->paginate = array(
+                'fields' => array('City.id', 'CONCAT(City.name," / ",City.population) as together'),
+                'link' => array(
+                    'State' => array(
+                        'fields' => array('name')
+                    )
+                )
+            );
+            
+            $this->DataTable->fields = array('City.id','0.together','State.name');
+            $this->DataTable->emptyElements = 1;
+            $this->set('cities', $this->DataTable->getResponse());
+            $this->set('_serialize','cities');
+        }
+    }
+    
+    public function virtualFields(){
+        if($this->RequestHandler->responseType() == 'json') {
+            
+            $this->City->virtualFields = array(
+                'together' => 'CONCAT(City.name," / ",City.population)'
+            );
+            
+            $this->paginate = array(
+                'fields' => array('City.id','City.together'),
+                'link' => array(
+                    'State' => array(
+                        'fields' => array('name')
+                    )
+                )
+            );
+            
+            $this->DataTable->fields = array('City.id','City.together','State.name');
+            $this->DataTable->emptyElements = 1;
+            $this->set('cities', $this->DataTable->getResponse());
+            $this->set('_serialize','cities');
+        }
+    }
+    
+    public function noJsonHandler(){
+        if($this->request->is('ajax')){
+            $this->autoRender = false;
+            $this->paginate = array(
+                'fields' => array('City.id', 'City.name', 'City.population'),
+                'link' => array(
+                    'State' => array(
+                        'fields' => array('name')
+                    )
+                )
+            );
 
+            $this->DataTable->fields = array('City.id','City.name','State.name','City.population');
+            $this->DataTable->emptyElements = 1;
+            echo json_encode($this->DataTable->getResponse());
         }
     }
 }
